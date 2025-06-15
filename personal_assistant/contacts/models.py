091105f099c123398d1209cfe2b4b301cfa7cab6
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-06-15 10:36:02 +0300

    add db

--- personal_assistant/contacts/models.py
+++ personal_assistant/contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    address = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150, unique=True)
+    phone_number = models.CharField(max_length=30, unique=True)
+    birthday = models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+
