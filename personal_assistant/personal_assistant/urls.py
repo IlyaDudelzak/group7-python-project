cdccd2ee2a0a8cb741cf7b9b889aac601c12bf6c
RaiBack21 <sviatoslavrybak@icloud.com>
2025-06-26 11:21:11 +0300

    Create profile page

--- personal_assistant/personal_assistant/urls.py
+++ personal_assistant/personal_assistant/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from news.views import base_view
 
@@ -28,4 +30,4 @@ urlpatterns = [
     path('notes/', include('notes.urls')),
     path('calendar/', include('diary.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
