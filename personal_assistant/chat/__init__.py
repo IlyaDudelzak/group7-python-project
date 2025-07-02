235b844d98b3a2387b11f694121a2f4aa180216e
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2025-07-02 21:21:06 +0300

    Update __init__.py

--- personal_assistant/chat/__init__.py
+++ personal_assistant/chat/__init__.py
@@ -1,15 +1,1 @@
-"""
-Chat app package initialization.
-"""
 
-from . import routes
-from . import models
-from . import forms
-from . import utils
-
-__all__ = [
-    "routes",
-    "models",
-    "forms",
-    "utils"
-]
